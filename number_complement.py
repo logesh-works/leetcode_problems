beece2b928a9a330b212407bad1e6c31bb577874
Logesh Kumar G <logeshkumar.dev@gmail.com>
2025-04-20 01:02:46 +0530

    20-04-2025

---
+++ number_complement.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        binary = bin(num)[2:]
+        bits = ''.join('1' if bit == '0' else '0' for bit in binary)
+        return int(bits, 2)
+         
